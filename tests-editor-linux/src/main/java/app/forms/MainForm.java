8cd899335da146facf4c7089d5735d9a6d936d50
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2020-10-23 12:39:39 +0300

    Форма для редактора тестов в linux

--- tests-editor-linux/src/main/java/app/forms/MainForm.java
+++ tests-editor-linux/src/main/java/app/forms/MainForm.java
@@ -1,2 +1,28 @@
-package app.forms;public class MainForm {
+package app.forms;
+
+import javax.swing.*;
+
+public class MainForm {
+    private JPanel panel1;
+    private JButton создатьТестButton;
+    private JButton открытьТестButton;
+    private JButton сохранитьТестButton;
+    private JButton сохранитьТестКакButton;
+    private JButton выполнитьТестButton;
+    private JButton выполнитьГруппуТестовButton;
+    private JButton сформироватьКомандуButton;
+    private JButton валидаторJsonButton;
+    private JButton обновитьWebDriverButton;
+    private JButton оПрограммеButton;
+    private JTabbedPane tabbedPane1;
+    private JTextField textField1;
+    private JTextField a9091TextField;
+    private JTextField textField2;
+    private JButton button1;
+    private JList list1;
+    private JButton button2;
+    private JButton uaButton;
+    private JButton icButton1;
+    private JTextField testHarTextField;
+    private JSpinner spinner1;
 }
