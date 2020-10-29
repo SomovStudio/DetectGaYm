1d784935dd91b93df7475643bfef886deb89b316
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2020-10-29 10:06:00 +0300

    На форму добавлны вкладки Данные и Шаги

--- tests-editor-linux/src/main/java/app/forms/MainForm.java
+++ tests-editor-linux/src/main/java/app/forms/MainForm.java
@@ -25,4 +25,18 @@ public class MainForm {
     private JButton icButton1;
     private JTextField testHarTextField;
     private JSpinner spinner1;
+    private JButton новаяЗаписьButton;
+    private JButton редактироватьЗаписьButton;
+    private JButton удалитьЗаписьButton;
+    private JButton копироватьЗаписьButton;
+    private JButton вверхButton;
+    private JButton внизButton;
+    private JTable table1;
+    private JButton новаяЗаписьButton1;
+    private JButton редактироватьЗаписьButton1;
+    private JButton удалитьЗаписьButton1;
+    private JButton копироватьЗаписьButton1;
+    private JButton вверхButton1;
+    private JButton внизButton1;
+    private JTable table2;
 }
