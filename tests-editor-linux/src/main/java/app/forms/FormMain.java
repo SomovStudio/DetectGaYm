b54bdc6435823e409306b87dc6f697bd3c7044f2
Somov <sep@zionec.ru>
2020-11-10 09:36:49 +0300

    Сформировано основное меню

--- tests-editor-linux/src/main/java/app/forms/FormMain.java
+++ tests-editor-linux/src/main/java/app/forms/FormMain.java
@@ -1,2 +1,22 @@
-package app.forms;public class FormMain {
+package app.forms;
+
+import javax.swing.*;
+
+public class FormMain {
+    private JPanel PanelMain;
+    private JMenuBar MenuBar;
+    private JMenu MenuFile;
+    private JMenu MenuExecute;
+    private JMenu MenuHelp;
+    private JMenu MenuReference;
+    private JMenuItem MenuCreateNewTest;
+    private JMenuItem MenuOpenTestFile;
+    private JMenuItem MenuSaveTestFile;
+    private JMenuItem MenuSaveAsTestFile;
+    private JMenuItem MenuExecuteTest;
+    private JMenuItem MenuExecuteGroupTest;
+    private JMenuItem MenuCreateCommand;
+    private JMenuItem MenuValidatorJson;
+    private JMenuItem MenuUpdateWedDriver;
+    private JMenuItem MenuAbout;
 }
