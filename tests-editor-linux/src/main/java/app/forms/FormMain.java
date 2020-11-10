0ac2f015ddeb6f35d92ae9808b27914823d7e3bb
Somov <sep@zionec.ru>
2020-11-10 10:15:19 +0300

    Перенесены все элементы формы

--- tests-editor-linux/src/main/java/app/forms/FormMain.java
+++ tests-editor-linux/src/main/java/app/forms/FormMain.java
@@ -1,6 +1,8 @@
 package app.forms;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FormMain {
     private JPanel PanelMain;
@@ -19,4 +21,49 @@ public class FormMain {
     private JMenuItem MenuValidatorJson;
     private JMenuItem MenuUpdateWedDriver;
     private JMenuItem MenuAbout;
+    private JTabbedPane tabbedPane1;
+    private JTextField textFieldDescription;
+    private JTextField textFieldPort;
+    private JTextField textFieldOpdion;
+    private JButton buttonOptionAdd;
+    private JList listOptions;
+    private JButton buttonOptionRemove;
+    private JButton buttonUA;
+    private JButton buttonIC;
+    private JTextField textFieldHar;
+    private JSpinner spinnerWaitLimit;
+    private JButton buttonDataAdd;
+    private JButton buttonDataEdit;
+    private JButton buttonDataDelete;
+    private JButton buttonDataCopy;
+    private JButton buttonDataUp;
+    private JButton buttonDataDown;
+    private JTable tableData;
+    private JButton buttonStepAdd;
+    private JButton buttonStepEdit;
+    private JButton buttonStepDelete;
+    private JButton buttonStepCopy;
+    private JButton buttonStepUp;
+    private JButton buttonStepDown;
+    private JTable tableSteps;
+    private JLabel labelPathFile;
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("MainForm");
+        frame.setContentPane(new FormMain().PanelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Editor tests for DetectGaYm");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public FormMain() {
+        MenuAbout.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null, "Editor for DetectGaYm");
+            }
+        });
+    }
+
+
 }
